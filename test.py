c98a766b5365329b974efe8f820ced94d7745cd3
heeyeon0501 <46666817+heeyeon0501@users.noreply.github.com>
2021-11-04 20:54:52 +0900

    Update test.py

--- test.py
+++ test.py
@@ -8,22 +8,16 @@ import random
 
 app = Flask(__name__)
 
-<<<<<<< HEAD
 
-=======
->>>>>>> 53d95d76438b13c03833dfa881738d21def10e4e
 # try:
 #     camera = cv2.VideoCapture(0)  # use 0 for web camera
 #
 # except:
 #     pass
-<<<<<<< HEAD
-=======
 
->>>>>>> 53d95d76438b13c03833dfa881738d21def10e4e
 
 # Load Yolo
-net = cv2.dnn.readNet("weights/yolov4-tiny.weights", "cfg/yolov4-tiny.cfg")
+net = cv2.dnn.readNet("weights/yolov3-tiny.weights", "cfg/yolov3-tiny.cfg")
 classes = []
 with open("coco.names", "r") as f:
     classes = [line.strip() for line in f.readlines()]
@@ -247,7 +241,4 @@ def gen_frames():  # generate frame by frame from camera
 if __name__ == '__main__':
 
     app.run(debug=False)
-<<<<<<< HEAD
-=======
 
->>>>>>> 53d95d76438b13c03833dfa881738d21def10e4e
